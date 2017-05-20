959bfba1940bbedc9a6b5161cc3029a950dc2f75
Shiratori_Shun <s1_boat@outlook.jp>
2017-05-20 18:14:28 +0900

    スキル準備

--- Assets/Script/MainScene/ItemDataBase.cs
+++ Assets/Script/MainScene/ItemDataBase.cs
@@ -6,17 +6,6 @@ public class ItemDataBase : MonoBehaviour
 {
     //リスト化をして下のvoid Start内でリストに値を追加
     public List<Item> items = new List<Item>();
-    //[SerializeField]private EffectDataBase database;
-
-/* 
-    void Start()    
-    {   
-        database.SetDataBase();
-        items.Add(new Item(null, 0, null, database.effects[0], 0, 0, Item.ItemType.Empty));//空用のダミーオブジェクト
-        items.Add(new Item("Bantage", 1, "傷の手当てに使う包帯。HPを30%回復する", database.effects[2], 30, 0, Item.ItemType.RecoverItem));
-        items.Add(new Item("Aidkit", 2, "傷の手当てに医療器具。HPを100%回復する", database.effects[3], 30, 0, Item.ItemType.RecoverItem));
-    }
-    */
     public void SetDataBase(EffectDataBase database)    
     {   
         items.Add(new Item(null, 0, null, database.effects[0], 0, 0, Item.ItemType.Empty));//空用のダミーオブジェクト
---
+++ Assets/Script/MainScene/Skill.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill
+{
+    public string skillName;        //名前
+    public int skillID;             //スキルID
+    public string skillDesc;        //スキルの説明文
+    public Texture2D skillIcon;     //アイコン
+    public int skillPower;          //効果の値。
+	public int needSkillID;			//必要の前提となるスキルID
+    public SkillType skillType;      //スキルの種類
+
+//アイテムタイプも同じくenum      
+    public enum SkillType
+    {
+        AttackUP,
+		DefenceUP,
+		SpeedUP,
+		HpUP,
+		InfectionUP,
+		InfectionStop,
+		IncreaseAction,
+		Other,
+    }
+//ここでリスト化時に渡す引数をあてがいます   
+public Skill(string name, int id, string desc,int power, SkillType type)
+    {
+        skillName = name;
+        skillID = id;
+//アイコンはnameとイコールにするのでアイコンがあるパス＋nameで取ってきます    
+        skillIcon = Resources.Load<Texture2D>("Textures/" + name);
+        skillDesc = desc;
+        skillPower = power;
+        skillType = type;
+    }
+}
