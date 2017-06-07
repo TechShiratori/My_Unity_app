d2652189df1f393ebaa3bc8b39dc268b09399e44
Shiratori_Shun <s1_boat@outlook.jp>
2017-06-07 09:03:10 +0900

    武器メニュー準備

---
+++ Assets/Script/PlayerMenu/WeaponList.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponList : MonoBehaviour {
+
+	[SerializeField] private Text m_weaponNameText;
+	[SerializeField] private Text m_weaponDesc;
+	[SerializeField] private RawImage m_weaponTexture;
+
+	// Use this for initialization
+	public void setWeapon(Weapon weapon){
+		m_weaponNameText.text = weapon.weaponName;
+		m_weaponDesc.text = weapon.weaponDesc;
+		m_weaponTexture.texture = Resources.Load<Texture2D>("WeaponIcon/" + weapon.weaponIconName);
+	}
+}
